76821ce28c93cdecdb5f479e1dca22318a771eb7
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-02-26 20:19:32 +0500

    Changes made

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import React from "react"
-
+import Button from '@material-ui/core/Button'
+import '../style.css'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div className="app" >
+    <h1 className="main-heading" >
+      Get Stuff Done
+    </h1>
+    <Button variant='contained' color='secondary' className='login-btn'  >Log In</Button>
+  </div>
 }
